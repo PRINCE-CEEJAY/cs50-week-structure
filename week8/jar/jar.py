15bd835d30298b5d41755e77632cacffe7bf7458
PRINCE CEEJAY <ceejaydroidprince@gmail.com>
2026-01-05 11:02:05 +0100

    corrected week8/jar

--- week8/jar/jar.py
+++ week8/jar/jar.py
@@ -1,20 +1,28 @@
 class Jar:
     def __init__(self, capacity=12):
-        ...
+        self.capacity = capacity
+        if self.capacity < 0:
+            raise ValueError("capacity must be a non-negative value")
 
     def __str__(self):
-        ...
+        return self.capacity * "🍪"
 
     def deposit(self, n):
-        ...
+        if n > self.capacity:
+            raise ValueError("Too much for the jar")
+        self.capacity += n
 
     def withdraw(self, n):
-        ...
+        if n > self.capacity:
+            raise ValueError("remaining cookies not up to that amount")
+        self.capacity -= n
+
 
     @property
     def capacity(self):
-        ...
+        return self.capacity
 
     @property
     def size(self):
-        ...
+        self.size = 0        
+        return self.size + self.capacity
